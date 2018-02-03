ae06bca93c51a5ca10cd8e2f4f19648c03fcc6af
Daphne Reed <daphne.reed@vidyard.com>
2018-02-03 09:29:21 -0500

    init

---
+++ tetris-gen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(void) {
+  srand(time(NULL));  
+  
+  char *piece[7]= {"F","S","I","Z","O","L","J"};
+  int randNum = rand() % 7;
+
+  printf (" Random piece: %s\n", piece[randNum]);
+ 
+  return 0;
+}
